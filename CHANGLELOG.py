0569a7c3dd0730624a5bfb18c6ec82ed547f7766
MiguelKristianBenitez <mkbbenitez@cmc.pshs.edu.ph>
2026-05-03 19:53:57 +0800

    Update v1.2.0 entry in CHANGLELOG.py

--- CHANGLELOG.py
+++ CHANGLELOG.py
@@ -1,6 +1,6 @@
 ### v1.0.0 - [05/03/26] - Added grading system
 (@Franxzine)
-### v1.2.0 - [05/03/26] - Added 
+### v1.2.0 - [05/03/26] - Added grading system
 (@MiguelKristianBenitez)
 ### v1.3.0 - [05/03/26] - Added changelog
 (@MishkaAngeliqueAguhob)
