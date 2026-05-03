1f67086335012d13aed8dfdf12d5e6d23b14c9e9
Franzxine <fgoabo@cmc.pshs.edu.ph>
2026-05-03 15:01:30 +0800

    Add functions to compute quarter grades and adjectives

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,1 +1,10 @@
+  def compute_quarter_grades(q1, t_q2, t_q3, t_q4):
+    Q1 = q1
+    Q2 = (Q1 + 2 * t_q2) / 3
+    Q3 = (Q2 + 2 * t_q3) / 3
+    Q4 = (Q3 + 2 * t_q4) / 3
+    return Q1, Q2, Q3, Q4
 
+
+def get_adjective(grade):
+    if 96 <= grade <= 100:
