58931f8256f1c83668d0f06f89c63a1611925397
Mishka Angelique Aguhob <magaguhob@cmc.pshs.edu.ph>
2026-05-03 20:14:29 +0800

    .

--- CHANGLELOG.py
+++ CHANGLELOG.py
@@ -1,1 +1,8 @@
+### v1.0.0 - [05/03/26] - Added grading system
+(@Franxzine)
 
+### v1.2.0 - [05/03/26] - Added tentative and quarter grade computation
+(@MiguelKristianBenitez)
+
+### v1.3.0 - [05/03/26] - Added grade adjective ratings
+(@MishkaAngeliqueAguhob)
