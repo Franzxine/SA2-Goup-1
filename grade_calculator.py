c44d3105d8fd60d80c84b392dd75fc3a119df5f5
Franzxine <fgoabo@cmc.pshs.edu.ph>
2026-05-03 15:38:36 +0800

    Update grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,1 +1,38 @@
+def compute_tentative(quiz, exam, project):
+    return (0.3 * quiz) + (0.4 * exam) + (0.3 * project)
 
+
+def compute_quarter_grades(q1, t_q2, t_q3, t_q4):
+    Q1 = q1
+    Q2 = (Q1 + 2 * t_q2) / 3
+    Q3 = (Q2 + 2 * t_q3) / 3
+    Q4 = (Q3 + 2 * t_q4) / 3
+    return Q1, Q2, Q3, Q4
+
+
+def get_adjective(grade):
+    if 96 <= grade <= 100:
+        return "EXCELLENT"
+    elif 90 <= grade:
+        return "VERY GOOD"
+    elif 84 <= grade:
+        return "VERY GOOD"
+    elif 78 <= grade:
+        return "GOOD"
+    elif 72 <= grade:
+        return "GOOD"
+    elif 66 <= grade:
+        return "SATISFACTORY"
+    elif 60 <= grade:
+        return "SATISFACTORY"
+    elif 55 <= grade:
+        return "FAIR"
+    elif 50 <= grade:
+        return "FAIR"
+    elif 40 <= grade:
+        return "FAILED ON CONDITION"
+    else:
+        return "FAILED"
+
+
+# Start here migz
